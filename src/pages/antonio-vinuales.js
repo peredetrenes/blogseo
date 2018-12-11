5d682d39e2d8a466e2e3007d1b1c2b58659b41cc
peredetrenes <45739157+peredetrenes@users.noreply.github.com>
2018-12-11 07:32:29 +0100

    Add files via upload

---
+++ src/pages/antonio-vinuales.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Layout from '../components/layout'
+const NewPage = () => (
+    <Layout>
+        <h1>Sobre mí</h1>
+		<p>Mi nombre es <strong>Antonio Viñuales</strong>. Soy profesor de Lengua castellana y Literatura en ESO, Bachillerato y Universidad. 
+		Trabajo en centros públicos del <a href="http://www.educaragon.org/">Gobierno de Aragón</a> y en la <a href="https://www.unizar.es/">Universidad de Zaragoza</a>. Soy licenciado en Filología Hispánica 
+		y máster en Teoría de la Literatura y Literatura comparada.</p>
+		<h2>Intereses</h2>
+		<p>Me interesan la filosofía, el arte, la publicidad, la sociología y la tecnología. Soy colaborador de revistas literarias 
+		como <a href="https://www.insula.es/">Ínsula (Revista de letras)</a> y <a href="http://www.ieturolenses.org/revista_turia/">Revista Turia</a>, 
+		y soy el editor de la Revista de literatura <a href="http://caminosdepakistan.es/"><i>Caminos de Pakistán</i></a>.</p>
+		<h3>Perfiles</h3>
+		<ul>
+		<li><a href="https://www.facebook.com/antonio.vinuales">facebook</a></li>
+		<li><a href="https://twitter.com/antoniovinuales">twitter</a></li>
+		<li><a href="https://www.instagram.com/peredetrenes/">Instagram</a></li>
+		<li><a href="https://www.youtube.com/c/AntonioVi%C3%B1ualesS%C3%A1nchez%20">Youtube</a></li>
+		<li><a href="https://medium.com/@antoniovinuales">Medium</a></li>
+		</ul>
+    </Layout>
+)
+export default NewPage
